b04fd5cc3155e5b5f96b1aecc6cb5e7d2240612c
Jesus Diaz <jeda.1992@hotmail.com>
2021-05-12 12:48:51 -0500

    modify next.config.js

--- next.config.js
+++ next.config.js
@@ -1,5 +1,5 @@
 module.exports = {
     images: {
-      domains: ['localhost', 'bejamas-jeda-test.herokuapp.com']
+      domains: ['localhost', 'bejamas-jeda-test.herokuapp.com', 'res.cloudinary.com']
     }
   }
